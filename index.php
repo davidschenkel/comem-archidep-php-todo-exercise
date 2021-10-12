8dbec5b8dfc6007885946b350da6a500866d1b1d
Salomé Gfeller <salome.gfeller@heig-vd.ch>
2021-10-12 14:10:17 +0200

    implementer toggle done

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/comem-archidep-php-todo-exercise/');
+define('BASE_URL', '/archidep/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
@@ -44,7 +44,9 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = "UPDATE todo SET done = !done WHERE id = $id;";
+
+
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -77,7 +79,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = "SELECT * FROM todo"; // IMPLEMENT ME
+$selectQuery = "SELECT * FROM todo";
 $items = $db->query($selectQuery);
 ?>
 
